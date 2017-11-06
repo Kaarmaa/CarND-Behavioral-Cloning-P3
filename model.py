a65cbde5d627bd79dd3a7c646f4cc9e11065f5a8
Kevin Krupinski <Krupinski.Kevin@gmail.com>
2017-11-06 23:32:00 +0100

    additional toying with better fixed for overfit + advanced map changes

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 
 correction = [0.0, 0.2, -0.2]
 lines = []
-batch_size = 128
+batch_size = 256
 
 # Loads Data from file into global buffer to allow multiple files to be loaded
 def getData(filename):
@@ -65,18 +65,24 @@ ch, row, col = 3, 160, 320  # Trimmed image format
 model = Sequential()
 model.add(Cropping2D(cropping=((70, 25), (0, 0)), input_shape=(row, col, ch)))
 model.add(Lambda(lambda x: x / 255.0 - 0.5))
-model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu", ))
 model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
 model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
 model.add(Convolution2D(64,3,3, activation="relu"))
 model.add(Convolution2D(64,3,3, activation="relu"))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Activation('elu'))
+model.add(Dropout(0.1))
 model.add(Dense(50))
+model.add(Activation('elu'))
+model.add(Dropout(0.1))
 model.add(Dense(10))
+model.add(Activation('elu'))
+model.add(Dropout(0.1))
 model.add(Dense(1))
 
-model.compile(loss='mse', optimizer='adam')
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 #model.fit_generator(X_train,y_train,validation_split=0.2, shuffle=True, nb_epoch=5, batch_size=512)
 # Keras2 requires steps
 steps_per_epoch = math.ceil(len(train_samples)/batch_size)
